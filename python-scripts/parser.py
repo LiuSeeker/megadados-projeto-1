93e8d8d861975cf7cf69e07c08593ee33b537406
LiuSeeker <vitor.liu@gmail.com>
2019-09-26 11:53:09 -0300

    ?

--- python-scripts/parser.py
+++ python-scripts/parser.py
@@ -5,9 +5,16 @@ def parser(text, key):
 
     for word in word_list:
         if(word[0] == key):
+            at_index = word.find("@", 1, len(word)-1)
+            if(at_index != -1):
+                word = word[:at_index]
             word = word.replace(',', '')
             word = word.replace('.', '')
             word = word.replace(';', '')
             return_list.append(word[1:])
 
     return return_list
+
+text = "pomeon@liu,,,#bicudo"
+
+print(parser(text, "@"))

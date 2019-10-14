a2ae3939368fd5527e27ed53e917c5d41d3d39bf
TeraByte900 <fe.n.teracini@hotmail.com>
2019-10-13 22:58:08 -0300

    funcoes_conjuntas novas

--- python_scripts/funcoes_conjuntas.py
+++ python_scripts/funcoes_conjuntas.py
@@ -25,3 +25,29 @@ def adiciona_post_e_mencoes(conn, id_usuario, titulo, texto, url_imagem):
 
     for k in mencoes_passaro_especies:
         adiciona_mencao_passaro(conn, id_post, k)
+
+
+def remove_post_e_mencoes(conn, id_post):
+
+    update_post_ativo(conn, id_post, 0)
+    remove_mencao_passaro_por_id_post(conn, id_post)
+    remove_mencao_usuario_por_id_post(conn, id_post)
+
+
+def update_post_texto_e_mencoes(conn, id_post, texto):
+
+    remove_mencao_passaro_por_id_post(conn, id_post)
+    remove_mencao_usuario_por_id_post(conn, id_post)
+
+    mencoes_usuario_usernames = parser1(texto, '@')
+    mencoes_passaro_especies = parser1(texto, '#')
+    mencoes_usuario_ids = []
+
+    for i in mencoes_usuario_usernames:
+        mencoes_usuario_ids.append(acha_usuario_id_por_username(conn, i))
+
+    for j in mencoes_usuario_ids:
+        adiciona_mencao_usuario(conn, id_post, j)
+
+    for k in mencoes_passaro_especies:
+        adiciona_mencao_passaro(conn, id_post, k)

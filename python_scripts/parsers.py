d55461e97f79aab369a496023d49c3ccdfab6670
TeraByte900 <fe.n.teracini@hotmail.com>
2019-10-13 23:54:41 -0300

    parsers

---
+++ python_scripts/parsers.py
@@ -0,0 +1,27 @@
+import re
+
+
+def parser1(text, key):
+    return_list = []
+    remove_list = ["@", "#", ",", ";", "."]
+
+    word_list = text.split(" ")
+
+    for word in word_list:
+        if(word[0] == key):
+            for remove_item in remove_list:
+                remove_index = word.find(remove_item, 1)
+                if(remove_index != -1):
+                    word = word[:remove_index]
+            return_list.append(word[1:])
+
+    return return_list
+
+
+def parser2(text, key):
+    return_list = []
+    word_list = re.findall("{}\w+".format(key), text)
+    for word in word_list:
+        return_list.append(word[1:])
+
+    return return_list

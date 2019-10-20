f16ab4147593a578ee9131f0ad6fedadaa506be1
LiuSeeker <vitor.liu@gmail.com>
2019-10-20 16:46:07 -0300

    triggers

--- scripts/trigger joinha.sql
+++ scripts/trigger joinha.sql
@@ -1,17 +1,18 @@
 USE rede_passaro;
 
+DROP TRIGGER IF EXISTS trig_update_anti_joinha;
 DROP TRIGGER IF EXISTS trig_update_pro_joinha;
 DROP TRIGGER IF EXISTS trig_update_joinha;
 
 DELIMITER //
-CREATE TRIGGER trig_update_pro_joinha
+CREATE TRIGGER trig_update_anti_joinha
 BEFORE UPDATE ON Joinha
 FOR EACH ROW
 BEGIN
 	IF NEW.pro_joinha = 1 THEN
-		SET @disale_trigger = 1;
         SET NEW.anti_joinha = 0;
-        SET @disale_trigger = 0;
+    ELSEIF NEW.anti_joinha = 1 THEN
+        SET NEW.pro_joinha = 0;
     END IF;
 END;//
 DELIMITER ;

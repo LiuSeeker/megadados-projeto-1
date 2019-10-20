c44158f10662f084f84e1ae98e6fa40a5f487ce2
LiuSeeker <vitor.liu@gmail.com>
2019-10-20 15:56:39 -0300

    conserta visualizacao

--- python_scripts/testes_visualizacao.py
+++ python_scripts/testes_visualizacao.py
@@ -42,6 +42,7 @@ class TestVisualizacao(unittest.TestCase):
         conn = self.__class__.connection
 
         adiciona_post(conn, 1, "titulo", "texto", "url_imagem")
+        agr = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT * FROM post')
             id_post = cursor.fetchone()[0]
@@ -49,18 +50,17 @@ class TestVisualizacao(unittest.TestCase):
         id_usuario = 2
         aparelho = 'iphone6'
         ip = '0.0.0.0'
-        instante = '1970-01-01 00:00:01'
         browser = 'chrome'
 
         res_esperado = [id_post, 2, 'iphone6', '0.0.0.0',
-                        datetime.datetime(1970, 1, 1, 0, 0, 1), browser]
+                        agr, browser]
 
         adiciona_visualizacao(conn, id_post, id_usuario,
-                              aparelho, ip, instante, browser)
+                              aparelho, ip, browser)
 
         try:
             adiciona_visualizacao(
-                conn, id_post, id_usuario, aparelho, ip, instante, browser)
+                conn, id_post, id_usuario, aparelho, ip, browser)
             self.fail(
                 'Nao deveria ter adicionado a mesma preferencia duas vezes.')
         except ValueError as e:
@@ -73,11 +73,13 @@ class TestVisualizacao(unittest.TestCase):
         conn = self.__class__.connection
 
         adiciona_post(conn, 1, "titulo", "texto", "url_imagem")
+        agr = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post = cursor.fetchone()[0]
 
         adiciona_post(conn, 2, "titulo", "texto", "url_imagem")
+        agr2 = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post2 = cursor.fetchone()[0]
@@ -85,22 +87,20 @@ class TestVisualizacao(unittest.TestCase):
         id_usuario = 2
         aparelho = 'iphone6'
         ip = '0.0.0.0'
-        instante = '1970-01-01 00:00:01'
         browser = 'chrome'
 
         adiciona_visualizacao(conn, id_post, id_usuario,
-                              aparelho, ip, instante, browser)
+                              aparelho, ip, browser)
 
         id_usuario2 = 1
         aparelho2 = 'iphone5'
         ip2 = '0.0.0.1'
-        instante2 = '1971-01-01 00:00:01'
 
         res_esperado = [id_post, 2, 'iphone6', '0.0.0.0',
-                        datetime.datetime(1970, 1, 1, 0, 0, 1), browser]
+                        agr, browser]
 
         adiciona_visualizacao(conn, id_post2, id_usuario2,
-                              aparelho2, ip2, instante2, browser)
+                              aparelho2, ip2, browser)
 
         res = lista_visualizacao_por_id_post(conn, id_post)
         self.assertCountEqual(res[0], res_esperado)
@@ -109,11 +109,13 @@ class TestVisualizacao(unittest.TestCase):
         conn = self.__class__.connection
 
         adiciona_post(conn, 1, "titulo", "texto", "url_imagem")
+        agr = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post = cursor.fetchone()[0]
 
         adiciona_post(conn, 2, "titulo", "texto", "url_imagem")
+        agr2 = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post2 = cursor.fetchone()[0]
@@ -121,22 +123,20 @@ class TestVisualizacao(unittest.TestCase):
         id_usuario = 2
         aparelho = 'iphone6'
         ip = '0.0.0.0'
-        instante = '1970-01-01 00:00:01'
         browser = 'chrome'
 
         adiciona_visualizacao(conn, id_post, id_usuario,
-                              aparelho, ip, instante, browser)
+                              aparelho, ip, browser)
 
         id_usuario2 = 1
         aparelho2 = 'iphone5'
         ip2 = '0.0.0.1'
-        instante2 = '1971-01-01 00:00:01'
 
         adiciona_visualizacao(conn, id_post2, id_usuario2,
-                              aparelho2, ip2, instante2, browser)
+                              aparelho2, ip2, browser)
 
         res_esperado = [id_post, 2, 'iphone6', '0.0.0.0',
-                        datetime.datetime(1970, 1, 1, 0, 0, 1), browser]
+                        agr, browser]
 
         res = lista_visualizacao_por_id_usuario(conn, 2)
         self.assertCountEqual(res[0], res_esperado)
@@ -145,11 +145,13 @@ class TestVisualizacao(unittest.TestCase):
         conn = self.__class__.connection
 
         adiciona_post(conn, 1, "titulo", "texto", "url_imagem")
+        agr = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post = cursor.fetchone()[0]
 
         adiciona_post(conn, 2, "titulo", "texto", "url_imagem")
+        agr2 = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post2 = cursor.fetchone()[0]
@@ -157,22 +159,20 @@ class TestVisualizacao(unittest.TestCase):
         id_usuario = 2
         aparelho = 'iphone6'
         ip = '0.0.0.0'
-        instante = '1970-01-01 00:00:01'
         browser = 'chrome'
 
         adiciona_visualizacao(conn, id_post, id_usuario,
-                              aparelho, ip, instante, browser)
+                              aparelho, ip, browser)
 
         id_usuario2 = 1
         aparelho2 = 'iphone5'
         ip2 = '0.0.0.1'
-        instante2 = '1971-01-01 00:00:01'
 
         adiciona_visualizacao(conn, id_post2, id_usuario2,
-                              aparelho2, ip2, instante2, browser)
+                              aparelho2, ip2, browser)
 
         res_esperado = [id_post, 2, 'iphone6', '0.0.0.0',
-                        datetime.datetime(1970, 1, 1, 0, 0, 1), browser]
+                        agr, browser]
 
         res = lista_visualizacao_por_aparelho(conn, 'iphone6')
         self.assertCountEqual(res[0], res_esperado)
@@ -181,11 +181,13 @@ class TestVisualizacao(unittest.TestCase):
         conn = self.__class__.connection
 
         adiciona_post(conn, 1, "titulo", "texto", "url_imagem")
+        agr = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post = cursor.fetchone()[0]
 
         adiciona_post(conn, 2, "titulo", "texto", "url_imagem")
+        agr2 = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post2 = cursor.fetchone()[0]
@@ -193,22 +195,20 @@ class TestVisualizacao(unittest.TestCase):
         id_usuario = 2
         aparelho = 'iphone6'
         ip = '0.0.0.0'
-        instante = '1970-01-01 00:00:01'
         browser = 'chrome'
 
         adiciona_visualizacao(conn, id_post, id_usuario,
-                              aparelho, ip, instante, browser)
+                              aparelho, ip, browser)
 
         id_usuario2 = 1
         aparelho2 = 'iphone5'
         ip2 = '0.0.0.1'
-        instante2 = '1971-01-01 00:00:01'
 
         adiciona_visualizacao(conn, id_post2, id_usuario2,
-                              aparelho2, ip2, instante2, browser)
+                              aparelho2, ip2, browser)
 
         res_esperado = [id_post, 2, 'iphone6', '0.0.0.0',
-                        datetime.datetime(1970, 1, 1, 0, 0, 1), browser]
+                        agr, browser]
 
         res = lista_visualizacao_por_ip(conn, '0.0.0.0')
         self.assertCountEqual(res[0], res_esperado)
@@ -217,6 +217,7 @@ class TestVisualizacao(unittest.TestCase):
         conn = self.__class__.connection
 
         adiciona_post(conn, 1, "titulo", "texto", "url_imagem")
+        agr = datetime.datetime.now().replace(microsecond=0)
         with conn.cursor() as cursor:
             cursor.execute('SELECT LAST_INSERT_ID()')
             id_post = cursor.fetchone()[0]
@@ -224,11 +225,10 @@ class TestVisualizacao(unittest.TestCase):
         id_usuario = 2
         aparelho = 'iphone6'
         ip = '0.0.0.0'
-        instante = '1970-01-01 00:00:01'
         browser = 'chrome'
 
         adiciona_visualizacao(conn, id_post, id_usuario,
-                              aparelho, ip, instante, browser)
+                              aparelho, ip, browser)
 
         remove_visualizacao(conn, id_post, id_usuario)
 
